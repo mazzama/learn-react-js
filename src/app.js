de32039ecdebb2bbe32ea15f51ad7c020f834296
Muhammad Azzam <mazzamabduljabbar@gmail.com>
2018-10-29 20:46:57 +0700

    create simple react boilerplate

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp  />, document.getElementById('app'));
+ReactDOM.render(<h1> This in my boilerplate</h1>, document.getElementById('app'));
