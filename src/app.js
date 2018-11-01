10fa5802afaa914b65a87f56837cb6c39e0543dc
Muhammad Azzam <mazzamabduljabbar@gmail.com>
2018-11-01 22:51:18 +0700

    integrating react and redux to app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const store = configureStore();
+
+const expense1 = store.dispatch(addExpense({description : 'Waterbill', amount: 1200}));
+const expense2 = store.dispatch(addExpense({description : 'Gasbill', amount: 1000}));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000)
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
+ReactDOM.render(jsx, document.getElementById('app'));
